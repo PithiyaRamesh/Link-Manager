bbc501993f013e65c549180c754c46eadb9578ac
Ramesh Pithiya <rameshp@hyperlinkinfosystem.net.in>
2023-10-03 21:03:53 +0530

    [-]Removed DSL tab layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -34,15 +36,10 @@ android {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
-//6.82MB
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
-
-    implementation ("com.github.angcyo.DslTablayout:TabLayout:3.5.5")
-    implementation ("com.github.angcyo.DslTablayout:ViewPager1Delegate:3.5.5")
-    implementation ("com.github.angcyo.DslTablayout:ViewPager2Delegate:3.5.5")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         google()
@@ -10,7 +12,6 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven("https://jitpack.io")
         mavenCentral()
     }
 }
